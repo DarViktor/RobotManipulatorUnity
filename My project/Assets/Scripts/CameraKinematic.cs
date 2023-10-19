9383db0c19f354e8a8777721c9d9d303c407b6bc
DarViktor <Vitya99uskov@gmail.com>
2023-10-19 15:52:29 +1000

    last commit

--- My project/Assets/Scripts/CameraKinematic.cs
+++ My project/Assets/Scripts/CameraKinematic.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class CameraKinematic : MonoBehaviour
 {
-    public RobotJoint[] Joints = new RobotJoint[6];
+    public RobotJoint[] Joints = new RobotJoint[7];
     // public HingeJoint[] hingeJoints = new HingeJoint[6];
-    public float[] Angles = new float[6];
+    private float[] Angles = new float[7];
 
     public Transform trigger;
     public Transform endEffector;
 
 
-    public float SamplingDistance = 1;
+    public float SamplingDistance = 0.1f;
     public float DistanceThreshold = 0.01f;
     public float LearningRate = 100;    
 
     public float DistanceWeight = 1;
-    public float RotationWeight = 1;
+    public float RotationWeight = 1f;
     public float TorsionWeight = 1;
 
-    private JointSpring[] spring = new JointSpring[6];
+    //private JointSpring[] spring = new JointSpring[6];
     void Start()
     {
         float[] angles = new float[Joints.Length];
@@ -48,7 +48,7 @@ public class CameraKinematic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InverseKinematics(trigger.position, Angles);
+        InverseKinematics(trigger.localPosition, Angles);
         for (int i = 0; i < Joints.Length; i++)
         {
             if (Joints[i].Axis.x == 1)
@@ -80,7 +80,19 @@ public class CameraKinematic : MonoBehaviour
             prevPoint = nextPoint;
         }
         //Debug.Log(prevPoint.y);
+        //Debug.Log(rotation.eulerAngles.x);
         return prevPoint;
+    }
+    public Vector3 ForwardRotationKinematics(float[] angles)
+    {
+        Quaternion rotation = Quaternion.identity;
+        for (int i = 1; i < Joints.Length; i++)
+        {
+            // Rotates around a new axis
+            rotation *= Quaternion.AngleAxis(angles[i - 1], Joints[i - 1].Axis);
+        }
+        //Debug.Log(rotation.eulerAngles);
+        return rotation.eulerAngles;
     }
 
     public float DistanceFromTarget(Vector3 target, float[] angles)
@@ -93,17 +105,18 @@ public class CameraKinematic : MonoBehaviour
     {
         Vector3 point = ForwardKinematics(angles);
         float maxDistance = 2.1f;
-        float distance = Vector3.Distance(point, target)/maxDistance;
+        float distance = Vector3.Distance(point, target) / maxDistance;
         return distance;
     }
 
     public float NormalisedRotation(Vector3 target, float[] angles)
     {
+        Vector3 rPosOrient = ForwardRotationKinematics(angles);
         float rotationPenalty =
         Mathf.Abs
         (
-         Quaternion.Angle(endEffector.rotation, trigger.rotation) / 180f
-        ) ;
+         Quaternion.Angle(Quaternion.Euler(new Vector3(rPosOrient.x, rPosOrient.y, rPosOrient.z)), Quaternion.Euler(new Vector3(trigger.eulerAngles.x, trigger.eulerAngles.y, trigger.eulerAngles.z))) / 360f
+        );
         return rotationPenalty;
     }
 
@@ -111,38 +124,26 @@ public class CameraKinematic : MonoBehaviour
     {
 
         float torsionPenalty = 0;
-        for (int i = 0; i< angles.Length; i++)
+        for (int i = 0; i < angles.Length; i++)
             torsionPenalty += Mathf.Abs(angles[i]);
         torsionPenalty /= angles.Length;
         return torsionPenalty / 360;
     }
     public float ErrorFunction(Vector3 target, float[] angles)
     {
-        Debug.Log(NormalisedRotation(target, angles));
-
-
+        //Debug.Log(NormalisedDistance(target, angles));
+        float distanceWeight = 1;
+        float rotationWeight = 1f;
+        float torsionWeight = 0.5f;
         return
-            NormalisedDistance(target, angles) * DistanceWeight +
-            NormalisedRotation(target, angles) * RotationWeight +
-            NormalisedTorsion(target, angles) * TorsionWeight;
+            NormalisedDistance(target, angles) * distanceWeight +
+            NormalisedRotation(target, angles) * rotationWeight +
+            NormalisedTorsion(target, angles) * torsionWeight;
     }
 
 
 
-    /*public float PartialGradient(Vector3 target, float[] angles, int i)
-    {
-        // Saves the angle,
-        // it will be restored later
-        float angle = angles[i];
-        // Gradient : [F(x+SamplingDistance) - F(x)] / h
-        float f_x = DistanceFromTarget(target, angles);
-        angles[i] += SamplingDistance;
-        float f_x_plus_d = DistanceFromTarget(target, angles);
-        float gradient = (f_x_plus_d - f_x) / SamplingDistance;
-        // Restores
-        angles[i] = angle;
-        return gradient;
-    }*/
+
     public float PartialGradient(Vector3 target, float[] angles, int i)
     {
         // Saves the angle,
@@ -155,29 +156,15 @@ public class CameraKinematic : MonoBehaviour
         float gradient = (f_x_plus_d - f_x) / SamplingDistance;
         // Restores
         angles[i] = angle;
+        if(i==4)Debug.Log(gradient);
+        //if (gradient < 0.0005 & gradient >-0.0005) gradient = 0;
         return gradient;
     }
 
-    /*public void InverseKinematics(Vector3 target, float[] angles)
-    {
-        if (DistanceFromTarget(target, angles) < DistanceThreshold)
-            return;
-        for (int i = Joints.Length - 1; i >= 0; i--)
-        {
-            // Gradient descent
-            // Update : Solution -= LearningRate * Gradient
-            float gradient = PartialGradient(target, angles, i);
-            angles[i] -= LearningRate * gradient;
-
-            // Clamp
-            angles[i] = Mathf.Clamp(angles[i], Joints[i].MinAngle, Joints[i].MaxAngle);
-            // Early termination
-            if (DistanceFromTarget(target, angles) < DistanceThreshold)
-                return;
-        }
-    }*/
     public void InverseKinematics(Vector3 target, float[] angles)
     {
+        //Debug.Log(ErrorFunction(target, angles));
+
         if (ErrorFunction(target, angles) < DistanceThreshold)
             return;
         for (int i = Joints.Length - 1; i >= 0; i--)
@@ -185,6 +172,8 @@ public class CameraKinematic : MonoBehaviour
             // Gradient descent
             // Update : Solution -= LearningRate * Gradient
             float gradient = PartialGradient(target, angles, i);
+            if (i == 4)
+                angles[i] += LearningRate * gradient;
             angles[i] -= LearningRate * gradient;
 
             // Clamp
@@ -194,4 +183,6 @@ public class CameraKinematic : MonoBehaviour
                 return;
         }
     }
+
+
 }
--- My project/Assets/Scripts/TargetKinematic.cs
+++ My project/Assets/Scripts/TargetKinematic.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class TargetKinematic : MonoBehaviour
 {
-    public RobotJoint[] Joints = new RobotJoint[6];
+    public RobotJoint[] Joints = new RobotJoint[7];
     // public HingeJoint[] hingeJoints = new HingeJoint[6];
-    public float[] Angles = new float[6];
+    public float[] Angles = new float[7];
 
     public Transform trigger;
 
@@ -35,13 +35,14 @@ public class TargetKinematic : MonoBehaviour
             }
         }
         Angles = angles;
-
-        LearningRate = 50;
+        
+        LearningRate = 70;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(Angles[5]);
         InverseKinematics(trigger.position, Angles);
         for (int i = 0; i < Joints.Length; i++)
         {
@@ -64,17 +65,29 @@ public class TargetKinematic : MonoBehaviour
     public Vector3 ForwardKinematics(float[] angles)
     {
         Vector3 prevPoint = Joints[0].transform.position;
+        //Vector3 prevRotation = 
         Quaternion rotation = Quaternion.identity;
         for (int i = 1; i < Joints.Length; i++)
         {
             // Rotates around a new axis
             rotation *= Quaternion.AngleAxis(angles[i - 1], Joints[i - 1].Axis);
             Vector3 nextPoint = prevPoint + rotation * Joints[i].StartOffset;
-
+          //  Vector3 nextRotation = prevRotation + rotation;
             prevPoint = nextPoint;
         }
-        //Debug.Log(prevPoint.y);
+        //Debug.Log(prevPoint);
         return prevPoint;
+    }
+    public Vector3 ForwardRotationKinematics(float[] angles)
+    {
+        Quaternion rotation = Quaternion.identity;
+        for (int i = 1; i < Joints.Length; i++)
+        {
+            // Rotates around a new axis
+            rotation *= Quaternion.AngleAxis(angles[i - 1], Joints[i - 1].Axis);          
+        }
+        //Debug.Log(rotation.eulerAngles);
+        return rotation.eulerAngles;
     }
 
     public float DistanceFromTarget(Vector3 target, float[] angles)
@@ -83,33 +96,83 @@ public class TargetKinematic : MonoBehaviour
         return Vector3.Distance(point, target);
     }
 
-    public Vector3 NormalisedDistance(Vector3 target, float[] angles)
+    public float NormalisedDistance(Vector3 target, float[] angles)
     {
         Vector3 point = ForwardKinematics(angles);
-        Vector3 distance = new Vector3(target.x - point.x, target.y - point.y, target.z - point.z);
-        return distance.normalized;
+        float maxDistance = 2.1f;
+        float distance = Vector3.Distance(point, target) / maxDistance;
+        return distance;
     }
-/*    float rotationPenalty =
-    Mathf.Abs
-    (
-     Quaternion.Angle(EndEffector.rotation, Destination.rotation) / 180fê
-    );
-    float torsionPenalty = 0;
-        for (int i = 0; i<solution.Length; i++)
-            torsionPenalty += Mathf.Abs(solution[i]);
-        torsionPenalty /= solution.Length;
-    public float ErrorFunction(Vector3 target, float[] angles)
+
+    public float NormalisedRotation(Vector3 target, float[] angles)
+    {
+        Vector3 rPosOrient = ForwardRotationKinematics(angles);
+        float rotationPenalty =
+        Mathf.Abs
+        (
+         Quaternion.Angle(Quaternion.Euler(new Vector3(rPosOrient.x, rPosOrient.y, rPosOrient.z)), Quaternion.Euler(new Vector3(trigger.eulerAngles.x, trigger.eulerAngles.y, trigger.eulerAngles.z))) / 360f
+        );
+        return rotationPenalty;
+    }
+
+    public float NormalisedTorsion(Vector3 target, float[] angles)
     {
 
+        float torsionPenalty = 0;
+        for (int i = 0; i < angles.Length; i++)
+            torsionPenalty += Mathf.Abs(angles[i]);
+        torsionPenalty /= angles.Length;
+        return torsionPenalty / 360;
+    }
+    public float ErrorFunction(Vector3 target, float[] angles)
+    {
+        //Debug.Log(NormalisedDistance(target, angles));
+        float distanceWeight = 1;
+        float rotationWeight = 0.8f;
+        float torsionWeight = 0;
         return
-            NormalisedDistance(target, angles) * DistanceWeight +
-            NormalisedRotation(target, angles) * RotationWeight +
-            NormalisedTorsion(target, angles) * TorsionWeight;
-    }*/
+            NormalisedDistance(target, angles) * distanceWeight /*+
+            NormalisedRotation(target, angles) * rotationWeight +
+            NormalisedTorsion(target, angles) * torsionWeight*/;
+    }
 
 
 
     public float PartialGradient(Vector3 target, float[] angles, int i)
+    {
+        // Saves the angle,
+        // it will be restored later
+        float angle = angles[i];
+        // Gradient : [F(x+SamplingDistance) - F(x)] / h
+        float f_x = ErrorFunction(target, angles);
+        angles[i] += SamplingDistance;
+        //if (i == 5) Debug.Log(angles[i]);
+        float f_x_plus_d = ErrorFunction(target, angles);
+        float gradient = (f_x_plus_d - f_x) / SamplingDistance;
+        // Restores
+        angles[i] = angle;
+        return gradient;
+    }
+
+    public void InverseKinematics(Vector3 target, float[] angles)
+    {
+        if (ErrorFunction(target, angles) < DistanceThreshold)
+            return;
+        for (int i = Joints.Length-1; i >= 0; i--)
+        {
+            // Gradient descent
+            // Update : Solution -= LearningRate * Gradient
+            float gradient = PartialGradient(target, angles, i);
+            angles[i] -= LearningRate * gradient;
+            //if (i == 5) Debug.Log(gradient);
+            // Clamp
+            angles[i] = Mathf.Clamp(angles[i], Joints[i].MinAngle, Joints[i].MaxAngle);
+            // Early termination
+            if (ErrorFunction(target, angles) < DistanceThreshold)
+                return;
+        }
+    }
+    /*public float PartialGradient(Vector3 target, float[] angles, int i)
     {
         // Saves the angle,
         // it will be restored later
@@ -141,5 +204,5 @@ public class TargetKinematic : MonoBehaviour
             if (DistanceFromTarget(target, angles) < DistanceThreshold)
                 return;
         }
-    }
+    }*/
 }
